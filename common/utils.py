c6f13631ed6e149c5c462ef882227e0dd2cf9d1d
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2019-11-02 15:18:41 +0200

    developing the small solution

---
+++ common/utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+import re
+import pandas as pd
+import os
+h = {
+    'Date': 0,
+    'Open': 1,
+    'High': 2,
+    'Low': 3,
+    'Close': 4,
+    'Volume': 5,
+    'OpenInt': 6
+}
+
+
+def get_potential(infile, method='maxrange'):
+    if method == 'maxrange':
+        # data = pd.read_csv(infile, header=0, index_col='Date')
+        data = np.genfromtxt(infile, dtype='str', delimiter=',')
+        if len(data) > 0:
+            data = data[1:]
+        else:
+        	print('Empty file: {}'.format(infile))
+        	return 0
+        # Now I need to find the max- min, where max_date > min_date
+        lmin = np.zeros(len(data), float)
+        rmax = np.zeros(len(data), float)
+        lmin[0] = float(data[0][h['Low']])
+        for i in np.arange(1, len(data)):
+            lmin[i] = min(float(data[i][h['Low']]), lmin[i-1])
+
+        rmax[-1] = float(data[-1][h['High']])
+        for i in np.arange(len(data)-2, -1, -1):
+            rmax[i] = max(float(data[i][h['High']]), rmax[i+1])
+
+        return np.max(rmax - lmin)
+
+
+def parse_stock_potential(indir):
+    # df = pd.DataFrame()
+    stocks = []
+    files = os.listdir(indir)
+    # files = ['flbr.us.txt']
+    # sys.stdout.write("[0/{}]".format(len(files)))
+    # sys.stdout.flush()
+    # sys.stdout.write("\b" * )
+    i = 0
+    for stockfile in files:
+	    print('[{}/{}]'.format(i, len(files)))
+        if 'us.txt' not in stockfile:
+            continue
+        name = stockfile.replace('.us.txt', '')
+        potential = get_potential(os.path.join(indir, stockfile))
+        stocks.append([name, potential])
+        i+=1
+    stocks = sorted(stocks, key=lambda a: a[1], reverse=True)
+    return stocks
--- scripts/small-time-travel.py
+++ scripts/small-time-travel.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.ticker
-from matplotlib.ticker import FormatStrFormatter
 import argparse
-import matplotlib.gridspec as gridspec
-from plot.plotting_utilities import *
-from matplotlib import colors
-from cycler import cycler
+import numpy as np
+import pandas as pd
 import sys
-import yaml
-import re
 import os
-import bisect
-from matplotlib import cm
-from common.utility import read_and_format_input
-
-# python ilp_analysis/ilp_percentiles.py -b results/LOOG-new/csvfiles/kernel-stats-saved_cycles_gtx1080ti_ptx0.22.10.19-11.51.40-merged.csv  -l results/suitability-analysis/csvfiles/kernel-stats-loog_new_inputs_gtx1080ti_ptx0.01.10.19-13.16.48-merged-merged.csv -o results/LOOG-new/plots/ -s -selected
-
+# import yaml
+# import re
+# import bisect
+from common import utils as ut
 
 this_directory = os.path.dirname(os.path.realpath(__file__)) + "/"
 this_filename = sys.argv[0].split('/')[-1]
@@ -25,229 +15,229 @@ this_filename = sys.argv[0].split('/')[-1]
 
 # Correlation plot with warp activity and total stalls.
 parser = argparse.ArgumentParser(
-    description='Various plots for the exploitable ILP analysis.',
+    description='Find solution for the small time travel problem.',
     usage='{} infile -o outfile'.format(this_filename))
 
-parser.add_argument('-l', '--loogfile', action='store', type=str,
-                    help='The input LOOG file that contains the raw counters.')
+parser.add_argument('-d', '--datadir', action='store', type=str,
+                    help='The input stock data dir.')
 
-parser.add_argument('-b', '--basefile', action='store', type=str,
-                    help='The input base file that contains the raw counters.')
+parser.add_argument('-m', '--moves', action='store', type=int,
+                    help='Max number of transactions.')
 
-# parser.add_argument('-t', '--tracefile', action='store', type=str,
-#                     help='The ILP trace input file.')
+parser.add_argument('-v', '--value', action='store', type=int,
+                    help='Min money value.')
 
-parser.add_argument('-o', '--outdir', action='store', type=str,
-                    default='./', help='The directory to store the plots.')
 
-parser.add_argument('-yg', '--yamlglobal', type=str,
-                    default='{}/configs/plot_config.yml'.format(
-                        this_directory),
-                    help='The global yaml config file.')
+parser.add_argument('-o', '--outfile', action='store', type=str,
+                    default='./small.txt', help='The file to store the small solution.')
 
-parser.add_argument('-yl', '--yamllocal', type=str,
-                    default='{}/configs/{}'.format(this_directory,
-                                                   this_filename.replace('.py', '.yml')),
-                    help='The local yaml config file.')
+# parser.add_argument('-yg', '--yamlglobal', type=str,
+#                     default='{}/common/config.yml'.format(
+#                         this_directory),
+#                     help='The global yaml config file.')
 
-parser.add_argument('-s', '--show', action='store_true',
-                    help='Show the plots or save only.')
-parser.add_argument('-selected', '--selected-kernels', action='store_true',
-                    help='Use the selected kernels.')
+# parser.add_argument('-s', '--show', action='store_true',
+#                     help='Show the plots or save only.')
+# parser.add_argument('-selected', '--selected-kernels', action='store_true',
+#                     help='Use the selected kernels.')
 
 if __name__ == '__main__':
     # read cmd line options
     args = parser.parse_args()
 
-    if not os.path.exists(args.outdir):
-        os.makedirs(args.outdir)
+    # if not os.path.exists(args.outdir):
+    #     os.makedirs(args.outdir)
 
     # read yaml config file
-    globyc = yaml.load(open(args.yamlglobal, 'r'),
-                       Loader=yaml.FullLoader)['global']
-    locyc = yaml.load(open(args.yamllocal, 'r'), Loader=yaml.FullLoader)
+    # globyc = yaml.load(open(args.yamlglobal, 'r'),
+    #                    Loader=yaml.FullLoader)
+    # locyc = yaml.load(open(args.yamllocal, 'r'), Loader=yaml.FullLoader)
 
-    # First I read all the input files
-    # Baseline data
-    retdic = read_and_format_input(locyc, globyc, vars(args))
-    basedic = retdic['basedic']
-    loogdic = retdic['loogdic']
-    all_kernels = retdic['all_kernels']
+    stock_potential = ut.parse_stock_potential(args.datadir)
 
-    if args.selected_kernels:
-        selected_kernels = globyc['selected_kernels']
-        diff = set(selected_kernels) - set(all_kernels)
-        if (len(diff) > 0):
-            print('WARNING: These kernels were not found')
-            print(diff)
-        all_kernels = list(set(selected_kernels) & set(all_kernels))
+    print(stock_potential[:10])
 
-    smetric = locyc['sortmetric']
-    pmetric = locyc['plotmetric']
+    # # First I read all the input files
+    # # Baseline data
+    # retdic = read_and_format_input(locyc, globyc, vars(args))
+    # basedic = retdic['basedic']
+    # loogdic = retdic['loogdic']
+    # all_kernels = retdic['all_kernels']
 
-    figdic = {}
-    # continue here, check that it works as expected
-    mixeddic = calc_mixed_metrics_with_scan(basedic, loogdic, locyc['mixedmetrics'],
-                                            formulas = globyc['mixed_formulas'],
-                                            constants = locyc['constants'],
-                                            knobs_to_keep=locyc['knobs_to_keep'])
+    # if args.selected_kernels:
+    #     selected_kernels = globyc['selected_kernels']
+    #     diff = set(selected_kernels) - set(all_kernels)
+    #     if (len(diff) > 0):
+    #         print('WARNING: These kernels were not found')
+    #         print(diff)
+    #     all_kernels = list(set(selected_kernels) & set(all_kernels))
 
-    # vals = [list(m2.values())[0] for m in mixeddic.values()
-    #         for m2 in m.values()]
-    # kernels = [list(m.keys())[0] for m in mixeddic.values()]
-    for k in all_kernels:
-        for m in basedic.keys():
-            for knob in basedic[m][k].keys():
-                if knob not in locyc['knobs_to_keep']:
-                    continue
-                name = locyc['knobs_to_keep'][knob]
-                if name not in figdic:
-                    figdic[name] = {'kernels':[]}
+    # smetric = locyc['sortmetric']
+    # pmetric = locyc['plotmetric']
 
-                if k not in figdic[name]['kernels']:
-                    figdic[name]['kernels'].append(k)
+    # figdic = {}
+    # # continue here, check that it works as expected
+    # mixeddic = calc_mixed_metrics_with_scan(basedic, loogdic, locyc['mixedmetrics'],
+    #                                         formulas = globyc['mixed_formulas'],
+    #                                         constants = locyc['constants'],
+    #                                         knobs_to_keep=locyc['knobs_to_keep'])
 
-                if m not in figdic[name]:
-                    figdic[name][m] = []
+    # # vals = [list(m2.values())[0] for m in mixeddic.values()
+    # #         for m2 in m.values()]
+    # # kernels = [list(m.keys())[0] for m in mixeddic.values()]
+    # for k in all_kernels:
+    #     for m in basedic.keys():
+    #         for knob in basedic[m][k].keys():
+    #             if knob not in locyc['knobs_to_keep']:
+    #                 continue
+    #             name = locyc['knobs_to_keep'][knob]
+    #             if name not in figdic:
+    #                 figdic[name] = {'kernels':[]}
 
-                figdic[name][m].append(basedic[m][k][knob])
-        for m in mixeddic.keys():
-            for knob in mixeddic[m][k].keys():
-                if knob not in locyc['knobs_to_keep']:
-                    continue
-                name = locyc['knobs_to_keep'][knob]
+    #             if k not in figdic[name]['kernels']:
+    #                 figdic[name]['kernels'].append(k)
 
-                if name not in figdic:
-                    figdic[name] = {'kernels':[]}
+    #             if m not in figdic[name]:
+    #                 figdic[name][m] = []
 
-                if k not in figdic[name]['kernels']:
-                    figdic[name]['kernels'].append(k)
+    #             figdic[name][m].append(basedic[m][k][knob])
+    #     for m in mixeddic.keys():
+    #         for knob in mixeddic[m][k].keys():
+    #             if knob not in locyc['knobs_to_keep']:
+    #                 continue
+    #             name = locyc['knobs_to_keep'][knob]
 
-                if m not in figdic[name]:
-                    figdic[name][m] = []
+    #             if name not in figdic:
+    #                 figdic[name] = {'kernels':[]}
 
-                figdic[name][m].append(mixeddic[m][k][knob])
+    #             if k not in figdic[name]['kernels']:
+    #                 figdic[name]['kernels'].append(k)
+
+    #             if m not in figdic[name]:
+    #                 figdic[name][m] = []
+
+    #             figdic[name][m].append(mixeddic[m][k][knob])
 
 
-    # I need to process the data and divide it into a list with #percentiles elements,
-    # and each element #aggregate_pos elements, this means I need to first sort the
-    # kernels according to the normalized IPC
-    for knob in figdic.keys():
-        idx = np.argsort(figdic[knob][smetric])
-        nans = np.zeros(len(idx), dtype=bool)
-        for m in figdic[knob].keys():
-            figdic[knob][m] = np.array(figdic[knob][m])[idx]
-            # if m != 'kernels':
-            #     nans = np.logical_or(nans, np.isnan(figdic[knob][m]))
-        for m in figdic[knob].keys():
-            figdic[knob][m] = figdic[knob][m][~nans]
-        print('Knob: {}, Kernels remained: {}'.format(
-            knob, len(idx)-np.sum(nans)))
+    # # I need to process the data and divide it into a list with #percentiles elements,
+    # # and each element #aggregate_pos elements, this means I need to first sort the
+    # # kernels according to the normalized IPC
+    # for knob in figdic.keys():
+    #     idx = np.argsort(figdic[knob][smetric])
+    #     nans = np.zeros(len(idx), dtype=bool)
+    #     for m in figdic[knob].keys():
+    #         figdic[knob][m] = np.array(figdic[knob][m])[idx]
+    #         # if m != 'kernels':
+    #         #     nans = np.logical_or(nans, np.isnan(figdic[knob][m]))
+    #     for m in figdic[knob].keys():
+    #         figdic[knob][m] = figdic[knob][m][~nans]
+    #     print('Knob: {}, Kernels remained: {}'.format(
+    #         knob, len(idx)-np.sum(nans)))
 
     
-    # sys.exit()
-    # Ok kernels have been sorted now, nans have been removed
-    # cmap = cm.get_cmap(figconf['percentiles']['colormap'])
-    # colors = [cmap(x) for x in np.linspace(0., 1., bins)]
-    for knob, vals in figdic.items():
-        figconf = locyc['figure']
-        fig, ax_arr = plt.subplots(**figconf['subplots'])
+    # # sys.exit()
+    # # Ok kernels have been sorted now, nans have been removed
+    # # cmap = cm.get_cmap(figconf['percentiles']['colormap'])
+    # # colors = [cmap(x) for x in np.linspace(0., 1., bins)]
+    # for knob, vals in figdic.items():
+    #     figconf = locyc['figure']
+    #     fig, ax_arr = plt.subplots(**figconf['subplots'])
 
-        # todo here use index
-        ax = ax_arr
-        plt.sca(ax)
+    #     # todo here use index
+    #     ax = ax_arr
+    #     plt.sca(ax)
 
-        step = 1.0
-        pos = 0
-        width = 0.95*step
-        # bins = figconf['percentiles']['number']
-        xticks = [[], []]
-        labels = set()
-        for k in vals['kernels']:
-            bottom = 0
-            for m in locyc['plotmetric']:
-                y = 100 * float(vals[m])
-                label = None
-                if m not in labels:
-                    label = globyc['metric_shorts'].get(m, m)
-                    labels.add(label)
+    #     step = 1.0
+    #     pos = 0
+    #     width = 0.95*step
+    #     # bins = figconf['percentiles']['number']
+    #     xticks = [[], []]
+    #     labels = set()
+    #     for k in vals['kernels']:
+    #         bottom = 0
+    #         for m in locyc['plotmetric']:
+    #             y = 100 * float(vals[m])
+    #             label = None
+    #             if m not in labels:
+    #                 label = globyc['metric_shorts'].get(m, m)
+    #                 labels.add(label)
 
-                plt.bar(pos, y, width=width, edgecolor='black',
-                    linewidth=1, color=locyc['colors'][m],
-                    hatch=locyc['hatches'][m], label=label)
+    #             plt.bar(pos, y, width=width, edgecolor='black',
+    #                 linewidth=1, color=locyc['colors'][m],
+    #                 hatch=locyc['hatches'][m], label=label)
 
-                if 'saved' not in m:
-                    bottom += y
+    #             if 'saved' not in m:
+    #                 bottom += y
 
 
-        # There should be only one knob
-        # extract the right subset of the array
-        # subIndexes = np.array_split(np.arange(len(vals[pmetric])), bins)
-        # title = 'Kernels: {}'.format(len(vals[pmetric]))
-        for i, subIndex in enumerate(subIndexes):
-            y = np.mean(vals[pmetric][subIndex])
-            x = np.mean(vals[smetric][subIndex])
-            plt.bar(pos, y, width=width, edgecolor='0',
-                    linewidth=1, color=colors[i],
-                    hatch=None, label=None)
-            xticks[0].append(pos)
-            xticks[1].append(x)
-            pos += step
-        # two more, one with IPC <= 1 and one with IPC > 1
-        plt.axvline(pos - 0.25*step, ymax=0.8,  ls='--', c='black')
-        pos += 0.5 * step
-        bless = 0
-        bmore = 0
-        idx_less = vals[smetric] <= 1
-        idx_more = vals[smetric] > 1
-        y = np.mean(vals[pmetric][idx_less])
-        plt.bar(pos, y, width=width, bottom=bless,
-                edgecolor='0', linewidth=1, color=colors[0],
-                hatch=None, label=None)
-        bless += y
+    #     # There should be only one knob
+    #     # extract the right subset of the array
+    #     # subIndexes = np.array_split(np.arange(len(vals[pmetric])), bins)
+    #     # title = 'Kernels: {}'.format(len(vals[pmetric]))
+    #     for i, subIndex in enumerate(subIndexes):
+    #         y = np.mean(vals[pmetric][subIndex])
+    #         x = np.mean(vals[smetric][subIndex])
+    #         plt.bar(pos, y, width=width, edgecolor='0',
+    #                 linewidth=1, color=colors[i],
+    #                 hatch=None, label=None)
+    #         xticks[0].append(pos)
+    #         xticks[1].append(x)
+    #         pos += step
+    #     # two more, one with IPC <= 1 and one with IPC > 1
+    #     plt.axvline(pos - 0.25*step, ymax=0.8,  ls='--', c='black')
+    #     pos += 0.5 * step
+    #     bless = 0
+    #     bmore = 0
+    #     idx_less = vals[smetric] <= 1
+    #     idx_more = vals[smetric] > 1
+    #     y = np.mean(vals[pmetric][idx_less])
+    #     plt.bar(pos, y, width=width, bottom=bless,
+    #             edgecolor='0', linewidth=1, color=colors[0],
+    #             hatch=None, label=None)
+    #     bless += y
 
-        y = np.mean(vals[pmetric][idx_more])
-        plt.bar(pos+step, y, width=width, bottom=bmore,
-                edgecolor='0', linewidth=1, color=colors[-1],
-                hatch=None, label=None)
-        bmore += y
+    #     y = np.mean(vals[pmetric][idx_more])
+    #     plt.bar(pos+step, y, width=width, bottom=bmore,
+    #             edgecolor='0', linewidth=1, color=colors[-1],
+    #             hatch=None, label=None)
+    #     bmore += y
 
-        xticks[0].append(pos)
-        xticks[1].append(np.mean(vals[smetric][idx_less]))
-        plt.text(pos, bless, '<1', fontsize=10, ha='center', va='bottom')
+    #     xticks[0].append(pos)
+    #     xticks[1].append(np.mean(vals[smetric][idx_less]))
+    #     plt.text(pos, bless, '<1', fontsize=10, ha='center', va='bottom')
 
-        xticks[0].append(pos+step)
-        xticks[1].append(np.mean(vals[smetric][idx_more]))
-        plt.text(pos+step, bmore, '>1', fontsize=10, ha='center', va='bottom')
+    #     xticks[0].append(pos+step)
+    #     xticks[1].append(np.mean(vals[smetric][idx_more]))
+    #     plt.text(pos+step, bmore, '>1', fontsize=10, ha='center', va='bottom')
 
-    plt.grid(axis='y', zorder=0, alpha=0.5)
-    # xticks = xtickspos.astype('str')
-    # xticks[-1] += '+'
-    xticks[1] = np.round(xticks[1], 2)
+    # plt.grid(axis='y', zorder=0, alpha=0.5)
+    # # xticks = xtickspos.astype('str')
+    # # xticks[-1] += '+'
+    # xticks[1] = np.round(xticks[1], 2)
 
-    # xticks[0] = np.array(xticks[0])[np.linspace(
-    #     0, len(xticks[0])-1, 5, dtype=int)]
-    # xticks[1] = np.round(np.linspace(xticks[1][0], xticks[1][-1], 5), 2)
-    plt.xticks(xticks[0], xticks[1], **figconf['ticks']['x'])
-    # plt.xlim(xticks[0][0]+(pos-width)/2 - (pos+width)/2,
-    #          xticks[0][-1]+(pos-width)/2 + (pos+width)/2)
-    plt.ylabel(figconf['ylabel'], **figconf['label'])
-    plt.xlabel(figconf['xlabel'], **figconf['label'])
-    ax.tick_params(**figconf['tick_params'])
-    # plt.legend(**figconf['legend'])
-    plt.tight_layout()
-    plt.subplots_adjust(**figconf['subplots_adjust'])
-    plt.ylim(figconf['ylim'][perwarp])
-    # plt.yticks(figconf['yticks'], **figconf['ticks']['y'])
-    # plt.title(title, **figconf['title'])
-    plt.text(s=title, transform=ax.transAxes, **figconf['text'])
+    # # xticks[0] = np.array(xticks[0])[np.linspace(
+    # #     0, len(xticks[0])-1, 5, dtype=int)]
+    # # xticks[1] = np.round(np.linspace(xticks[1][0], xticks[1][-1], 5), 2)
+    # plt.xticks(xticks[0], xticks[1], **figconf['ticks']['x'])
+    # # plt.xlim(xticks[0][0]+(pos-width)/2 - (pos+width)/2,
+    # #          xticks[0][-1]+(pos-width)/2 + (pos+width)/2)
+    # plt.ylabel(figconf['ylabel'], **figconf['label'])
+    # plt.xlabel(figconf['xlabel'], **figconf['label'])
+    # ax.tick_params(**figconf['tick_params'])
+    # # plt.legend(**figconf['legend'])
+    # plt.tight_layout()
+    # plt.subplots_adjust(**figconf['subplots_adjust'])
+    # plt.ylim(figconf['ylim'][perwarp])
+    # # plt.yticks(figconf['yticks'], **figconf['ticks']['y'])
+    # # plt.title(title, **figconf['title'])
+    # plt.text(s=title, transform=ax.transAxes, **figconf['text'])
 
-    outfiles = ['{}/{}-{}-kernels{}_bins{}.jpeg'.format(args.outdir,
-                                                        this_filename.replace('.py', ''), perwarp, len(all_kernels), bins),
-                '{}/{}-{}-kernels{}_bins{}.pdf'.format(args.outdir,
-                                                       this_filename.replace('.py', ''), perwarp, len(all_kernels),  bins)]
-    for outfile in outfiles:
-        save_and_crop(fig, outfile, dpi=600, bbox_inches='tight')
-    if args.show:
-        plt.show()
-    plt.close()
+    # outfiles = ['{}/{}-{}-kernels{}_bins{}.jpeg'.format(args.outdir,
+    #                                                     this_filename.replace('.py', ''), perwarp, len(all_kernels), bins),
+    #             '{}/{}-{}-kernels{}_bins{}.pdf'.format(args.outdir,
+    #                                                    this_filename.replace('.py', ''), perwarp, len(all_kernels),  bins)]
+    # for outfile in outfiles:
+    #     save_and_crop(fig, outfile, dpi=600, bbox_inches='tight')
+    # if args.show:
+    #     plt.show()
+    # plt.close()
