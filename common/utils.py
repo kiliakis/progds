b9b14dc9ee80cc5dcbc71cf1882cacc35f50591b
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2019-11-03 11:50:20 +0100

    added preprocess script

--- common/utils.py
+++ common/utils.py
@@ -12,53 +12,76 @@ h = {
     'OpenInt': 6
 }
 
+# h = {
+#     'Date': 'datetime64[s]',
+#     'Open': 'f4',
+#     'High': 'f4',
+#     'Low': 'f4',
+#     'Close': 'f4',
+#     'Volume': 'i4',
+#     'OpenInt': 'i4'
+# }
 
-def get_potential(infile, method='maxrange'):
+
+
+def get_potential(infile, method='maxr'):
     data = np.genfromtxt(infile, dtype='str', delimiter=',')
-    if method == 'maxrange':
-        if len(data) > 0:
-            data = data[1:]
-        else:
-            print('Empty file: {}'.format(infile))
-            return 0
+    if len(data) > 0:
+        data = data[1:]
+    else:
+        print('Empty file: {}'.format(infile))
+        return []
+    
+    # data = np.array(data, dtype={'names': list(h.keys()), 'formats': list(h.values())})
+    if method == 'maxr':
         # data = pd.read_csv(infile, header=0, index_col='Date')
         # Now I need to find the max- min, where max_date > min_date
         lmin = np.zeros(len(data), float)
         rmax = np.zeros(len(data), float)
-        lmin[0] = float(data[0][h['Low']])
+        lmin[0] = float(data[0, h['Low']])
         for i in np.arange(1, len(data)):
-            lmin[i] = min(float(data[i][h['Low']]), lmin[i-1])
+            lmin[i] = min(float(data[i, h['Low']]), lmin[i-1])
 
         rmax[-1] = float(data[-1][h['High']])
         for i in np.arange(len(data)-2, -1, -1):
-            rmax[i] = max(float(data[i][h['High']]), rmax[i+1])
+            rmax[i] = max(float(data[i, h['High']]), rmax[i+1])
+        rmax = rmax - lmin
+        idx = np.argmax(rmax)
+        return [data[idx, h['Date']], rmax[idx]]
+    elif method == 'subd':
+        # I need some cheap stocks, I need to know when it was less
+        # than a dollar, and if after that it increased
+        dollar_i = -1
+        for i, d in enumerate(data):
+            if float(d[h['Low']]) <= 1:
+                dollar_i = i
+                break
+        if dollar_i < 0:
+            # this means that it was never less than 1usd
+            return []
+        # else I need to find the max value, after this index
+        max_val = np.array(data[dollar_i:, h['High']], float)
+        max_val = np.max(max_val)
+        return [data[dollar_i, h['Date']], max_val]
+    elif method == 'dp':
+        return []
 
-        return np.max(rmax - lmin)
-    elif method == 'subdollar':
-        if len(data) > 0:
-            data = data[1:]
-        else:
-            print('Empty file: {}'.format(infile))
-            return 0
-        # I need some cheap stocks
 
-def parse_stock_potential(indir):
+def parse_stock_potential(indir, method='maxr', numstocks=-1):
     # df = pd.DataFrame()
     stocks = []
     files = os.listdir(indir)
-    # files = ['flbr.us.txt']
-    # sys.stdout.write("[0/{}]".format(len(files)))
-    # sys.stdout.flush()
-    # sys.stdout.write("\b" * )
+    if numstocks > 0:
+        files = files[:numstocks]
     i = 0
     for stockfile in files:
         print('[{}/{}]'.format(i, len(files)))
         if 'us.txt' not in stockfile:
             continue
         name = stockfile.replace('.us.txt', '')
-        potential = get_potential(os.path.join(indir, stockfile), method='maxrange')
-        potential = get_potential(os.path.join(indir, stockfile), method='subdollar')
-        stocks.append([name, potential])
-        i+=1
-    stocks = sorted(stocks, key=lambda a: a[1], reverse=True)
+        potential = get_potential(os.path.join(indir, stockfile), method=method)
+        if len(potential) > 0:
+            stocks.append([name] + potential)
+        i += 1
+    stocks = sorted(stocks, key=lambda a: a[-1], reverse=True)
     return stocks
